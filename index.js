3a27ac345c790cc67cba4758019b1afbaa1973b1
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-01 09:53:26 -0700

    add Pseudo code and DIRECTIONS

---
+++ index.js
@@ -0,0 +1,35 @@
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+
+// Pseduo code for HW
+// npm init
+// install dependencies - inquirer
+
+// require all dependencies inquirer and fs
+
+// create and array of questions
+
+// writing a README in a markdown file as a  template
+
+// function will generate README template
+
+// use inquirer to prompt users with questions
+
+// use answers that come back from inquirer - pass those into my generate readme function
+
+// write file using template generated fromREADME function
