8515e4fb666c9418c33238987a0a4298e6e590ba
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-01 23:04:22 -0700

    write basic necessary function

--- index.js
+++ index.js
@@ -40,7 +40,69 @@ inq.prompt ([
         message: "Please enter your Email address",
         name: "Email"
     },
-])
+]).then(function (response) {
+    `# ${response.Title}
+
+    ## Description 
+    ${response.Description}
+    
+    
+    
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+      
+    
+    ## Installation
+    ${response.Installation}
+
+      * [inquirer](https://www.npmjs.com/package/inquirer) that will prompt you for your inputs from the command line 
+      * [axios](https://www.npmjs.com/package/axios) to fetch your info from the GitHub API
+    
+    
+    ## Usage 
+    
+    *Instructions and examples for use:*
+    
+    ![Gif demo of README-generator](readme-demo.gif)
+    
+    
+    The application then takes your responses and uses axios to fetch your GitHub profile from the [GitHub API](https://developer.github.com/v3/), including your GitHub profile picture (avatar) and email.
+    From there, the application will generate markdown and a table of contents for the README conditionally based on your responses to the Inquirer prompts (so, if you don't answer the optional questions, such as Installation, an Installation section will not be included in your README).
+    
+    Finally, fs.writeFile is used to generate your project's README.md file. 
+    
+    
+    ## License
+
+    ${response.License}
+    
+    This application is provided under an MIT License.
+    
+    ---
+    
+    ## Contributing
+    
+    ${response.Contribution}
+    
+    ## Tests
+    
+    Right now there is no testing enabled
+    
+    ## Questions?
+    
+    
+    
+    GitHub: ${response.GitHub}
+    
+    Email: ${response.Email} `
+
+    console.log(response.Title);
+    
+})
 
 
 // array of questions for user
