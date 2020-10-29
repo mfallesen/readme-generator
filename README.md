1a4eb5402d9d38dc9329744c8797dbed8d076f1d
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-28 22:25:07 -0700

    create filewrite

--- README.md
+++ README.md
@@ -1,55 +1,57 @@
-# README.md Generator
+# This is a test
 
-## Description 
-${description}
+    ## Description 
+    this is just a test readme
+    
+    
+    
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+      
+    
+    ## Installation
+    npm pleasedontinstallthis
 
+      * [inquirer](https://www.npmjs.com/package/inquirer) that will prompt you for your inputs from the command line 
+    
+    
+    ## Usage 
+    
+    *Instructions and examples for use:*
+    
+    ![Gif demo of README-generator](readme-demo.gif)
+    
+    
+    The application then takes your responses and uses axios to fetch your GitHub profile from the [GitHub API](https://developer.github.com/v3/), including your GitHub profile picture (avatar) and email.
+    From there, the application will generate markdown and a table of contents for the README conditionally based on your responses to the Inquirer prompts (so, if you don't answer the optional questions, such as Installation, an Installation section will not be included in your README).
+    
+    Finally, fs.writeFile is used to generate your project's README.md file. 
+    
+    
+    ## License
 
-
-## Table of Contents
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-  
-
-## Installation
-
-  * [`inquirer`](https://www.npmjs.com/package/inquirer) that will prompt you for your inputs from the command line 
-  * [`axios`](https://www.npmjs.com/package/axios) to fetch your info from the GitHub API
-
-
-## Usage 
-
-*Instructions and examples for use:*
-
-![Gif demo of README-generator](readme-demo.gif)
-
-
-The application then takes your responses and uses `axios` to fetch your GitHub profile from the [GitHub API](https://developer.github.com/v3/), including your GitHub profile picture (avatar) and email.
-From there, the application will generate markdown and a table of contents for the README conditionally based on your responses to the Inquirer prompts (so, if you don't answer the optional questions, such as Installation, an Installation section will not be included in your README).
-
-Finally, `fs.writeFile` is used to generate your project's README.md file. 
-
-
-## License
-
-This application is provided under an MIT License.
-
----
-
-## Contributing
-
-This project is not accepting contributions at this time however feel free poke around the code and hit me up with questions or comments.
-
-## Tests
-
-Right now there is no testing enabled
-
-## Questions?
-
-
-
-GitHub: 
-
-Email: 
+    
+    
+    This application is provided under an MIT License.
+    
+    ---
+    
+    ## Contributing
+    
+    its not very good
+    
+    ## Tests
+    
+    Right now there is no testing enabled
+    
+    ## Questions?
+    
+    
+    
+    GitHub: mfallesen
+    
+    Email: mfallesen2010@gmail.com 
--- index.js
+++ index.js
@@ -1,47 +1,58 @@
 const fs = require("fs")
 const inq = require("inquirer");
-const Choices = require("inquirer/lib/objects/choices");
 
+function init() {
 
-inq.prompt ([
-    {
-        type: "input",
-        message: "What would you like to title this README?",
-        name: "Title"
-    },
-    {
-        type: "input",
-        message: "Please describe this project.",
-        name: "Description"
-    },
-    {
-        type: "input",
-        message: "How is this project installed?",
-        name: "Installation"
-    },
-    {
-        type: "input",
-        message: "Please enter guidelines for people who wish to contribute to this project.",
-        name: "Contribution"
-    },
-    {
-        type: "checkbox",
-        message: "What license do you want to release this software under?",
-        name: "License",
-        choices: ["MIT"]
-    },
-    {
-        type: "input",
-        message: "Enter your GitHub UserName",
-        name: "GitHub"
-    },
-    {
-        type: "input",
-        message: "Please enter your Email address",
-        name: "Email"
-    },
-]).then(function (response) {
-    `# ${response.Title}
+    inq.prompt([
+        {
+            type: "input",
+            message: "Please enter a file name for this README. (You can leave out the extension)",
+            name: "FileName"
+        },
+        {
+            type: "input",
+            message: "What would you like to title this README?",
+            name: "Title"
+        },
+        {
+            type: "input",
+            message: "Please describe this project.",
+            name: "Description"
+        },
+        {
+            type: "input",
+            message: "How is this project installed?",
+            name: "Installation"
+        },
+        {
+            type: "input",
+            message: "How is this project intended to be used?",
+            name: "Usage"
+        },
+        {
+            type: "input",
+            message: "Please enter guidelines for people who wish to contribute to this project.",
+            name: "Contribution"
+        },
+        {
+            type: "checkbox",
+            message: "What license do you want to release this software under?",
+            name: "License",
+            choices: ["MIT"]
+        },
+        {
+            type: "input",
+            message: "Enter your GitHub UserName",
+            name: "GitHub"
+        },
+        {
+            type: "input",
+            message: "Please enter your Email address",
+            name: "Email"
+        },
+    ]).then(function (response) {
+        const markdownTemplate = `
+    # ${response.Title}
 
     ## Description 
     ${response.Description}
@@ -60,7 +71,6 @@ inq.prompt ([
     ${response.Installation}
 
       * [inquirer](https://www.npmjs.com/package/inquirer) that will prompt you for your inputs from the command line 
-      * [axios](https://www.npmjs.com/package/axios) to fetch your info from the GitHub API
     
     
     ## Usage 
@@ -68,19 +78,11 @@ inq.prompt ([
     *Instructions and examples for use:*
     
     ![Gif demo of README-generator](readme-demo.gif)
-    
-    
-    The application then takes your responses and uses axios to fetch your GitHub profile from the [GitHub API](https://developer.github.com/v3/), including your GitHub profile picture (avatar) and email.
-    From there, the application will generate markdown and a table of contents for the README conditionally based on your responses to the Inquirer prompts (so, if you don't answer the optional questions, such as Installation, an Installation section will not be included in your README).
-    
-    Finally, fs.writeFile is used to generate your project's README.md file. 
-    
+    ${response.Usage}
     
     ## License
-
-    ${response.License}
     
-    This application is provided under an MIT License.
+    This application is provided under an ${response.License} License.
     
     ---
     
@@ -93,50 +95,30 @@ inq.prompt ([
     Right now there is no testing enabled
     
     ## Questions?
-    
-    
-    
-    GitHub: ${response.GitHub}
+    Get in touch.
+    GitHub: https://github.com/${response.GitHub}
     
     Email: ${response.Email} `
 
-    console.log(response.Title);
-    
-})
+        console.log(response.Title);
+        writeToFile(`${response.fileName}.md`, markdownTemplate);
+    })
 
+    // Readme file writer
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, function (err) {
 
-// array of questions for user
-const questions = [
+            if (err) {
+                return console.log(err);
+            }
 
+            console.log("Markdown File Successfully generated!");
 
-];
-console.log(questions)
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
+        });
+    }
 
 }
 
-// function call to initialize program
 init();
 
 
-// Pseduo code for HW
-// npm init
-
-
-// create and array of questions
-
-// writing a README in a markdown file as a  template
-
-// function will generate README template
-
-// use inquirer to prompt users with questions
-
-// use answers that come back from inquirer - pass those into my generate readme function
-
-// write file using template generated fromREADME function
